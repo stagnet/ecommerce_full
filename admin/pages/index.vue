c14b1234b502557ab2a05be4801b0736b3cdf76b
abhi <52235699+stagnet@users.noreply.github.com>
2021-01-03 18:05:42 +0530

    admin front end

---
+++ admin/pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <main>
+    <div class="container">
+      <h1>All Products</h1>
+      <vs-row>
+        <vs-col>
+          <vs-button active color="#f4d078" gradient
+            >Add a New Product
+            <template #animate>
+              <i class="bx bxs-shopping-bags"></i>
+            </template>
+          </vs-button>
+        </vs-col>
+        <vs-col>
+          <vs-button active color="#f4d078" gradient
+            >Add a New Product
+            <template #animate>
+              <i class="bx bxs-shopping-bags"></i>
+            </template>
+          </vs-button>
+        </vs-col>
+        <vs-col>
+          <vs-button active color="#f4d078" gradient
+            >Add a New Product
+            <template #animate>
+              <i class="bx bxs-shopping-bags"></i>
+            </template> </vs-button
+        ></vs-col>
+      </vs-row>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
