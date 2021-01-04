f55d61466e0f3366a2d5c1bbb2599dfde5ff3d5e
abhi <52235699+stagnet@users.noreply.github.com>
2021-01-04 23:57:12 +0530

    added input inside product form

--- admin/pages/product.vue
+++ admin/pages/product.vue
@@ -1,9 +1,34 @@
 <template>
-  <div></div>
+  <main>
+    <div class="container">
+      <vs-row>
+        <vs-col w="3" sm="3"></vs-col>
+        <vs-col w="6" sm="6">
+          <section>
+            <h2 style="text-align: center" class="sub-heading">
+              Add a New Product
+            </h2>
+            <form>
+              <div class="a-spacing-medium">
+                <vs-input label-placeholder="Category" v-model="category" />
+              </div>
+            </form>
+          </section>
+        </vs-col>
+        <vs-col w="3" sm="3"></vs-col>
+      </vs-row>
+    </div>
+  </main>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      category: '',
+    }
+  },
+}
 </script>
 
 <style scoped></style>
