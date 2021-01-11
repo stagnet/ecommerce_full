f9a0a0bd9bea298f52cf84083faef9f6c840fbd4
abhi <52235699+stagnet@users.noreply.github.com>
2021-01-11 10:01:07 +0530

    post product using form

--- admin/pages/product.vue
+++ admin/pages/product.vue
@@ -7,7 +7,7 @@
           <h2 style="text-align: center" class="sub-heading">
             Add a New Product
           </h2>
-          <form>
+          <form @submit.prevent="AddProductData">
             <!-- category -->
             <div class="a-spacing-medium">
               <vs-select
@@ -68,6 +68,17 @@
               />
             </div>
 
+            <!-- stock Quantity -->
+            <div class="a-spacing-medium">
+              <vs-input
+                type="text"
+                label-placeholder="Stocks Quantity"
+                v-model="stockQuantity"
+                color="#e77600"
+                class="inputs"
+              />
+            </div>
+
             <!-- Description -->
             <div class="a-spacing-medium">
               <label class="bookDescription"> Description </label>
@@ -75,6 +86,7 @@
                 class="inputs"
                 placeholder="Provide details such as a product description"
                 style="padding: 5px 10px; border-radius: 12px"
+                v-model="description"
               >
               </textarea>
             </div>
@@ -85,17 +97,17 @@
               <div class="a-row">
                 <label class="choosefile-button">
                   <i class="fa fa-plus" aria-hidden="true"></i>
-                  <input type="file" />
+                  <input type="file" @change="onFileSelected" />
                 </label>
                 <p style="margin-top: 10px; font-size: 15px">
-                  Name of the photo
+                  {{ fileName }}
                 </p>
               </div>
             </div>
 
             <!-- submit button -->
             <div class="a-spacing-large">
-              <vs-button color="#f0c14b" relief gradient circle>
+              <vs-button color="#f0c14b" relief gradient circle upload>
                 <strong>Upload</strong>
                 <i class="bx bx-upload bx-sm"></i>
               </vs-button>
@@ -134,18 +146,43 @@ export default {
       console.error(error)
     }
   },
-
   // ? data() is processed on client-side(browser) while asynData() process in server-side..
   data() {
     return {
-      categoryID: null,
-      ownerID: null,
+      category: '',
+      owner: '',
       title: '',
       price: 0,
+      stockQuantity: 1,
       selectedFile: null,
       description: '',
       fileName: '',
     }
+  },
+
+  methods: {
+    onFileSelected(event) {
+      this.selectedFile = event.target.files[0]
+      console.log(this.selectedFile)
+      this.fileName = event.target.files[0].name
+    },
+    //? post data to db..
+    async AddProductData() {
+      let data = new FormData()
+      data.append('title', this.title)
+      data.append('price', this.price)
+      data.append('description', this.description)
+      data.append('ownerID', this.owner)
+      data.append('stockQuantity', this.stockQuantity)
+      data.append('categoryID', this.category)
+      data.append('photo', this.selectedFile, this.selectedFile.name)
+
+      let result = await this.$axios.$post(
+        'http://localhost:3000/api/products',
+        data
+      )
+      this.$router.push('/')
+    },
   },
 }
 </script>
